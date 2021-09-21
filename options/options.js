4f73b2529e5a965c509dee0c49cac38d03466c69
Crowdbotics <team@crowdbotics.com>
2021-09-21 08:14:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "soft_grass_30718Identifier",
+  url: "https://soft_grass_30718Identifier.botics.co",
+  api: "https://soft_grass_30718Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
